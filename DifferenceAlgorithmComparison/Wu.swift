b897392bb9ec3ac03a0166d74d38068d62b735e5
horita-yuya <horita_yuya@cyberagent.co.jp>
2018-03-14 13:35:33 +0900

    :hammer: add source snaking calculation

--- DifferenceAlgorithmComparison/Wu.swift
+++ DifferenceAlgorithmComparison/Wu.swift
@@ -2,11 +2,9 @@
 //  Wu.swift
 //  DifferenceAlgorithmComparison
 //
-//  Created by 堀田 有哉 on 2018/02/17.
-//  Copyright © 2018年 yuyahorita. All rights reserved.
+//  Created by 堀田 有哉 on 2018/02/09.
+//  Copyright © 2018年 hy. All rights reserved.
 //
-
-import Foundation
 
 struct Wu<E: Equatable> {
     enum GraphScript: CustomStringConvertible, Equatable {
@@ -31,7 +29,7 @@ struct Wu<E: Equatable> {
             }
         }
         
-        static func ==(lhs: GraphScript, rhs: GraphScript) -> Bool {
+        static func == (lhs: GraphScript, rhs: GraphScript) -> Bool {
             switch (lhs, rhs) {
             case let (.delete(lfi), .delete(rfi)):
                 return lfi == rfi
@@ -51,7 +49,7 @@ struct Wu<E: Equatable> {
         }
     }
     
-    static func diff(from fromArray: Array<E>, to toArray: Array<E>) -> Array<GraphScript> {
+    static func diff(from fromArray: [E], to toArray: [E]) -> [GraphScript] {
         if fromArray.count == 0 && toArray.count == 0 {
             return []
         } else if fromArray.count == 0 && toArray.count > 0 {
@@ -71,11 +69,11 @@ struct Wu<E: Equatable> {
 private extension Wu {
     typealias Edge = (P: Int, from: Vertice, to: Vertice, script: GraphScript, snakeCount: Int)
     
-    static func reverseTree(path: Array<Edge>, sinkVertice: Vertice) -> Array<GraphScript> {
+    static func reverseTree(path: [Edge], sinkVertice: Vertice) -> [GraphScript] {
         var nextToVertice = sinkVertice
-        var scripts = Array<GraphScript>()
+        var scripts: [GraphScript] = []
         
-        path.reversed().forEach { D, fromVertice, toVertice, script, snakeCount in
+        path.reversed().forEach { _, fromVertice, toVertice, script, snakeCount in
             guard toVertice.snakeOffset(by: snakeCount) == nextToVertice else { return }
             nextToVertice = fromVertice
             
@@ -91,7 +89,7 @@ private extension Wu {
         return scripts
     }
     
-    static func exploreEditGraph(from fromArray: Array<E>, to toArray: Array<E>, isInversed: Bool = false) -> Array<Edge> {
+    static func exploreEditGraph(from fromArray: [E], to toArray: [E], isInversed: Bool = false) -> [Edge] {
         let fromCount = fromArray.count
         let toCount = toArray.count
         if fromCount > toCount { return exploreEditGraph(from: toArray, to: fromArray, isInversed: true) }
@@ -99,7 +97,7 @@ private extension Wu {
         let totalCount = toCount + fromCount
         let delta = toCount - fromCount
         var furthestReaching = Array(repeating: -1, count: totalCount + 2)
-        var path = Array<Edge>()
+        var path: [Edge] = []
         
         let snake: (Int, Int) -> Int = { k, y in
             var _y = y
@@ -120,44 +118,36 @@ private extension Wu {
                 _y = furthestReaching[index + 1]
                 fromVertice = .vertice(x: _y - k - 1, y: _y)
                 toVertice = .vertice(x: _y - k, y: _y)
-                script = .delete(at: _y - k - 1)
+                let inversedScript: GraphScript = _y == 0 ? .insertToHead(from: _y - k - 1) : .insert(from: _y - k - 1, to: _y - 1)
+                let defaultScript: GraphScript = .delete(at: _y - k - 1)
+                script = isInversed ? inversedScript : defaultScript
             } else {
                 _y = furthestReaching[index - 1] + 1
                 fromVertice = .vertice(x: _y - k, y: _y - 1)
                 toVertice = .vertice(x: _y - k, y: _y)
-                script = _y - k == 0 ? .insertToHead(from: _y - 1) : .insert(from: _y - 1, to: _y - k - 1)
+                let inversedScript: GraphScript = .delete(at: _y - 1)
+                let defaultScript: GraphScript = _y - k == 0 ? .insertToHead(from: _y - 1) : .insert(from: _y - 1, to: _y - k - 1)
+                script = isInversed ? inversedScript : defaultScript
             }
             
             return (_y: _y, fromVertice: fromVertice, toVertice: toVertice, script: script)
         }
         
-        let inversedEdgeMove: (Int) -> (_y: Int, fromVertice: Vertice, toVertice: Vertice, script: GraphScript) = { k in
+        let headForFurthest: (Int, Int) -> Void = { k, p in
             let index = k + fromCount
-            let _y: Int
-            let fromVertice: Vertice
-            let toVertice: Vertice
-            let script: GraphScript
-            
-            if furthestReaching[index - 1] + 1 <= furthestReaching[index + 1] {
-                _y = furthestReaching[index + 1]
-                fromVertice = .vertice(x: _y - k - 1, y: _y)
-                toVertice = .vertice(x: _y - k, y: _y)
-                script = _y == 0 ? .insertToHead(from: _y - k - 1) : .insert(from: _y - k - 1, to: _y - 1)
-            } else {
-                _y = furthestReaching[index - 1] + 1
-                fromVertice = .vertice(x: _y - k, y: _y - 1)
-                toVertice = .vertice(x: _y - k, y: _y)
-                script = .delete(at: _y - 1)
-            }
-            
-            return (_y: _y, fromVertice: fromVertice, toVertice: toVertice, script: script)
-        }
-        
-        let headForFurthest: (Int, Int) -> () = { k, p in
-            let index = k + fromCount
-            let (_y, fromVertice, toVertice, script) = isInversed ? inversedEdgeMove(k) : edgeMove(k)
+            let (_y, fromVertice, toVertice, script) = edgeMove(k)
             furthestReaching[index] = snake(k, _y)
             path.append((P: p, from: fromVertice, to: toVertice, script: script, snakeCount: furthestReaching[index] - _y))
+        }
+        
+        let sourceSnaking: () -> Void = {
+            furthestReaching[fromCount] = snake(0, 0)
+            path.append((P: 0,
+                         from: .vertice(x: 0, y: 0),
+                         to: .vertice(x: furthestReaching[fromCount], y: furthestReaching[fromCount]),
+                         script: .sourceScript,
+                         snakeCount: furthestReaching[fromCount])
+            )
         }
         
         for p in 0...fromCount {
@@ -165,8 +155,7 @@ private extension Wu {
                 let lowerRange = -p...delta - 1
                 for k in lowerRange {
                     if p == 0 && k == 0 {
-                        furthestReaching[fromCount] = snake(0, 0)
-                        path.append((P: 0, from: .vertice(x: 0, y: 0), to: .vertice(x: furthestReaching[fromCount], y: furthestReaching[fromCount]), script: .sourceScript, snakeCount: furthestReaching[fromCount]))
+                        sourceSnaking()
                     } else {
                         headForFurthest(k, p)
                     }
@@ -180,8 +169,7 @@ private extension Wu {
             
             let deltaIndex = delta + fromCount
             if p == 0 && delta == 0 {
-                furthestReaching[deltaIndex] = snake(0, 0)
-                path.append((P: 0, from: .vertice(x: 0, y: 0), to: .vertice(x: furthestReaching[fromCount], y: furthestReaching[fromCount]), script: .sourceScript, snakeCount: furthestReaching[fromCount]))
+                sourceSnaking()
             } else {
                 headForFurthest(delta, p)
             }
@@ -202,10 +190,11 @@ private extension Wu {
             return .vertice(x: x + count, y: y + count)
         }
         
-        static func ==(lhs: Vertice, rhs: Vertice) -> Bool {
+        static func == (lhs: Vertice, rhs: Vertice) -> Bool {
             guard case let .vertice(lx, ly) = lhs,
                 case let .vertice(rx, ry) = rhs else { return false }
             return lx == rx && ly == ry
         }
     }
 }
+
